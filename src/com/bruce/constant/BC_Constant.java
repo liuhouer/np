fca862c8d5fe234d29ddb041e7d202f1062aa053
zhangyang <654714226@qq.com>
2015-12-17 23:21:00 +0800

    修改公共位

--- src/com/bruce/constant/BC_Constant.java
+++ src/com/bruce/constant/BC_Constant.java
@@ -4,24 +4,6 @@ package com.bruce.constant;
 
 public class BC_Constant {
 	
-	/** 课程分类  */
-	public static enum CourseType {
-		TUIJIAN("推荐课程",0),BENXIAO("本校课程",1),GONGGONG("公共课程",2);
-	    
-	    private final String key;  
-	    private final int value;  
-	    private CourseType(String key, int value) {  
-	        this.key = key;  
-	        this.value = value;  
-	    }  
-	   
-	    public String getKey() {  
-	        return key;  
-	    }  
-	    public int getValue() {  
-	        return value;  
-	    }  
-	}
 	
 	/** 是否启用*/
 	public static enum Enabled {
