51f9f9c5285118eb2b6a95f1312d7f46b34aa5af
zhangyang <654714226@qq.com>
2016-07-04 09:37:22 +0800

    daiely

--- src/main/java/com/bruce/utils/HTMLParserUtil.java
+++ src/main/java/com/bruce/utils/HTMLParserUtil.java
@@ -535,7 +535,7 @@ public class HTMLParserUtil
 				Elements dates = doc_.select("span[class=activity-meta no-extra]");
 				String date = dates.get(0).text();
 				
-				if(!(date.equals("2016-06-29"))){
+				if(!(date.equals("2016-07-02")) && !(date.equals("2016-07-03"))){
 					continue;
 				}
 				
