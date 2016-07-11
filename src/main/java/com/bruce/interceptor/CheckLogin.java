e5327cd3fed256010ef8a4bfb3283872c40be975
zhangyang <654714226@qq.com>
2016-07-11 17:41:48 +0800

    添加登陆拦截器|实现注解式拦截

---
+++ src/main/java/com/bruce/interceptor/CheckLogin.java
@@ -0,0 +1,26 @@
+
+package com.bruce.interceptor;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 登陆校验|此注解写在用于登录权限的Controller上面
+ * @author bruce
+ * @date 2016年7月11日
+ * @email zhangyang226@gmail.com
+ * @site http://blog.northpark.cn | http://northpark.cn | orginazation https://github.com/jellyband
+ * 
+ */
+@Documented
+@Inherited
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CheckLogin {
+	
+	boolean validate() default true;
+}
