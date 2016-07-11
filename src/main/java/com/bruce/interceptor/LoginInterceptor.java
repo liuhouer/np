d61e9e844a1d31ac35b525ffa6c9db623f2b7f29
zhangyang <654714226@qq.com>
2016-07-11 19:24:50 +0800

    j

--- src/main/java/com/bruce/interceptor/LoginInterceptor.java
+++ src/main/java/com/bruce/interceptor/LoginInterceptor.java
@@ -6,7 +6,6 @@ package com.bruce.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.ui.Model;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
