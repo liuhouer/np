d256819651524ff990b8c0a255e9c7819db5af11
zhangyang <654714226@qq.com>
2016-07-11 17:40:55 +0800

    添加登陆拦截器|实现注解式拦截

--- src/main/java/com/bruce/interceptor/LoginInterceptor.java
+++ src/main/java/com/bruce/interceptor/LoginInterceptor.java
@@ -6,9 +6,12 @@ package com.bruce.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.ui.Model;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import com.bruce.constant.BC_Constant;
 import com.bruce.model.User;
 
 /**
@@ -17,29 +20,41 @@ import com.bruce.model.User;
  *
  */
 public class LoginInterceptor extends HandlerInterceptorAdapter {
-    private static final String[] IGNORE_URI = {"/login.jsp", "/login","/toLogin","backui/","frontui/"};
  
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        boolean flag = false;
-        String url = request.getRequestURL().toString();
-        System.out.println(">>>: " + url);
-        for (String s : IGNORE_URI) {
-            if (url.contains(s)) {
-                flag = true;
-                break;
-            }
-        }
-        if (!flag) {
-            User user = (User) request.getSession().getAttribute("user");
-			if(user!=null){
-				
-			}else{
-				response.sendRedirect("/login?url="+url);
-			}
-        }
-        return flag;
-    }
+        
+    	
+    	if(handler.getClass().isAssignableFrom(HandlerMethod.class)){
+    		CheckLogin checklogin = ((HandlerMethod) handler).getMethodAnnotation(CheckLogin.class);
+
+    		//没有声明需要权限,或者声明不验证权限
+    		if(checklogin == null || checklogin.validate() == false){
+    			return true;
+    		}else{                
+    			//在这里实现自己的权限验证逻辑
+    			User user = (User) request.getSession().getAttribute("user");
+    			if(user!=null){//如果验证成功返回true（这里直接写false来模拟验证失败的处理）
+    				return true;
+    			}else{//如果验证失败
+    				//返回到登录界面
+    				String url = request.getRequestURL().toString();
+    				String[] strs = url.split("8082/");
+    				String postfix = strs[1];
+    				url = "http://"+BC_Constant.Domain+"/"+postfix;
+    				request.setAttribute("redirectURI", url);
+    				response.sendRedirect("/login?redirectURI="+url);
+    				return false;
+    			}       
+    		}
+    	}else{
+    		return true;   
+    	}
+    	 
+    	 
+      }
+    	
+    	
  
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
