9eecc42917662b1c35c7f4432242111c1fed4ec2
zhangyang <654714226@qq.com>
2016-07-24 12:28:49 +0800

    防sql注入

--- src/main/java/cn/northpark/utils/safe/SqlInjection.java
+++ src/main/java/cn/northpark/utils/safe/SqlInjection.java
@@ -2,12 +2,31 @@ package cn.northpark.utils.safe;
 
 public class SqlInjection{
 	
-  public String escapeSql(String content){
-	  String flt ="'|and|exec|insert|select|delete|update|count|*|%|chr|mid|master|truncate|char|declare|; |or|-|+|,"; 
-	  String filter[] = flt.split("|"); 
-	  for(int i=0; i <filter.length;i++){
-		  content.replace(filter[i], ""); 
-	  }
-	  return content; 
+  public static String escapeSql(String str){
+	  str = str.isEmpty() ? "" : str.replaceAll("'", "''");
+      str = str.replaceAll("(?i)exec",  "&#101;xec");
+      str = str.replaceAll("(?i)xp_cmdshell",  "&#120;p_cmdshell");
+      str = str.replaceAll("(?i)select",  "&#115;elect");
+      str = str.replaceAll("(?i)insert",  "&#105;nsert");
+      str = str.replaceAll("(?i)update",  "&#117;pdate");
+      str = str.replaceAll("(?i)delete",  "&#100;elete");
+      str = str.replaceAll("(?i)drop",  "&#100;rop");
+      str = str.replaceAll("(?i)create",  "&#99;reate");
+      str = str.replaceAll("(?i)rename",  "&#114;ename");
+      str = str.replaceAll("(?i)truncate",  "&#116;runcate");
+      str = str.replaceAll("(?i)alter",  "&#97;lter");
+      str = str.replaceAll("(?i)exists",  "&#101;xists");
+      str = str.replaceAll("(?i)master.",  "&#109;aster.");
+      str = str.replaceAll("(?i)restore",  "&#114;estore");
+      str = str.replaceAll("(?i)where",  "&#117;here");
+      
+      System.out.println("sql_inj_handle-------->"+str);
+      return str;
   }
+  
+  public static void main(String[] args) {
+	escapeSql("select * from bc_movies where 1=1");
+  }
+  
+  
 }
