a45635e74bad98952c65cddd5cc3295502bc6659
zhangyang <654714226@qq.com>
2016-07-26 18:16:27 +0800

    lunbo

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -75,7 +75,7 @@ $(function(){
 				},5000);
 				/***不需要自动滚动，去掉即可***/
 				linum = $('.mainlist li').length;//图片数量
-				w = linum * 250;//ul宽度
+				w = linum * 150;//ul宽度
 				$('.piclist').css('width', w + 'px');//ul宽度
 				$('.swaplist').html($('.mainlist').html());//复制内容
 				
@@ -85,19 +85,19 @@ $(function(){
 						$('.swaplist,.mainlist').stop(true,true);
 					}
 					
-					if($('.mainlist li').length>4){//多于4张图片
+					if($('.mainlist li').length>6){//多于6张图片
 						ml = parseInt($('.mainlist').css('left'));//默认图片ul位置
 						sl = parseInt($('.swaplist').css('left'));//交换图片ul位置
 						if(ml<=0 && ml>w*-1){//默认图片显示时
-							$('.swaplist').css({left: '1000px'});//交换图片放在显示区域右侧
-							$('.mainlist').animate({left: ml - 1000 + 'px'},'slow');//默认图片滚动				
-							if(ml==(w-1000)*-1){//默认图片最后一屏时
+							$('.swaplist').css({left: '1200px'});//交换图片放在显示区域右侧
+							$('.mainlist').animate({left: ml - 900 + 'px'},'slow');//默认图片滚动				
+							if(ml==(w-900)*-1){//默认图片最后一屏时
 								$('.swaplist').animate({left: '0px'},'slow');//交换图片滚动
 							}
 						}else{//交换图片显示时
-							$('.mainlist').css({left: '1000px'})//默认图片放在显示区域右
-							$('.swaplist').animate({left: sl - 1000 + 'px'},'slow');//交换图片滚动
-							if(sl==(w-1000)*-1){//交换图片最后一屏时
+							$('.mainlist').css({left: '1200px'})//默认图片放在显示区域右
+							$('.swaplist').animate({left: sl - 900 + 'px'},'slow');//交换图片滚动
+							if(sl==(w-900)*-1){//交换图片最后一屏时
 								$('.mainlist').animate({left: '0px'},'slow');//默认图片滚动
 							}
 						}
@@ -109,18 +109,18 @@ $(function(){
 						$('.swaplist,.mainlist').stop(true,true);
 					}
 					
-					if($('.mainlist li').length>4){
+					if($('.mainlist li').length>6){
 						ml = parseInt($('.mainlist').css('left'));
 						sl = parseInt($('.swaplist').css('left'));
 						if(ml<=0 && ml>w*-1){
 							$('.swaplist').css({left: w * -1 + 'px'});
-							$('.mainlist').animate({left: ml + 1000 + 'px'},'slow');				
+							$('.mainlist').animate({left: ml + 900 + 'px'},'slow');				
 							if(ml==0){
-								$('.swaplist').animate({left: (w - 1000) * -1 + 'px'},'slow');
+								$('.swaplist').animate({left: (w - 900) * -1 + 'px'},'slow');
 							}
 						}else{
-							$('.mainlist').css({left: (w - 1000) * -1 + 'px'});
-							$('.swaplist').animate({left: sl + 1000 + 'px'},'slow');
+							$('.mainlist').css({left: (w - 900) * -1 + 'px'});
+							$('.swaplist').animate({left: sl + 900 + 'px'},'slow');
 							if(sl==0){
 								$('.mainlist').animate({left: '0px'},'slow');
 							}
@@ -216,19 +216,19 @@ $(function(){
 //			$('.swaplist,.mainlist').stop(true,true);
 //		}
 //		
-//		if($('.mainlist li').length>4){//多于4张图片
+//		if($('.mainlist li').length>6){//多于6张图片
 //			ml = parseInt($('.mainlist').css('left'));//默认图片ul位置
 //			sl = parseInt($('.swaplist').css('left'));//交换图片ul位置
 //			if(ml<=0 && ml>w*-1){//默认图片显示时
-//				$('.swaplist').css({left: '1000px'});//交换图片放在显示区域右侧
-//				$('.mainlist').animate({left: ml - 1000 + 'px'},'slow');//默认图片滚动				
-//				if(ml==(w-1000)*-1){//默认图片最后一屏时
+//				$('.swaplist').css({left: '900px'});//交换图片放在显示区域右侧
+//				$('.mainlist').animate({left: ml - 900 + 'px'},'slow');//默认图片滚动				
+//				if(ml==(w-900)*-1){//默认图片最后一屏时
 //					$('.swaplist').animate({left: '0px'},'slow');//交换图片滚动
 //				}
 //			}else{//交换图片显示时
-//				$('.mainlist').css({left: '1000px'})//默认图片放在显示区域右
-//				$('.swaplist').animate({left: sl - 1000 + 'px'},'slow');//交换图片滚动
-//				if(sl==(w-1000)*-1){//交换图片最后一屏时
+//				$('.mainlist').css({left: '900px'})//默认图片放在显示区域右
+//				$('.swaplist').animate({left: sl - 900 + 'px'},'slow');//交换图片滚动
+//				if(sl==(w-900)*-1){//交换图片最后一屏时
 //					$('.mainlist').animate({left: '0px'},'slow');//默认图片滚动
 //				}
 //			}
@@ -240,18 +240,18 @@ $(function(){
 //			$('.swaplist,.mainlist').stop(true,true);
 //		}
 //		
-//		if($('.mainlist li').length>4){
+//		if($('.mainlist li').length>6){
 //			ml = parseInt($('.mainlist').css('left'));
 //			sl = parseInt($('.swaplist').css('left'));
 //			if(ml<=0 && ml>w*-1){
 //				$('.swaplist').css({left: w * -1 + 'px'});
-//				$('.mainlist').animate({left: ml + 1000 + 'px'},'slow');				
+//				$('.mainlist').animate({left: ml + 900 + 'px'},'slow');				
 //				if(ml==0){
-//					$('.swaplist').animate({left: (w - 1000) * -1 + 'px'},'slow');
+//					$('.swaplist').animate({left: (w - 900) * -1 + 'px'},'slow');
 //				}
 //			}else{
-//				$('.mainlist').css({left: (w - 1000) * -1 + 'px'});
-//				$('.swaplist').animate({left: sl + 1000 + 'px'},'slow');
+//				$('.mainlist').css({left: (w - 900) * -1 + 'px'});
+//				$('.swaplist').animate({left: sl + 900 + 'px'},'slow');
 //				if(sl==0){
 //					$('.mainlist').animate({left: '0px'},'slow');
 //				}
