27b63fb161a3d443639373439e20dcdcfa771b48
liuhouer <654714226@qq.com>
2018-11-19 13:40:22 +0800

    add cookie utils

---
+++ src/main/java/cn/northpark/utils/CookieUtil.java
@@ -0,0 +1,44 @@
+package cn.northpark.utils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created on 2018/4/14 
+ *
+ */
+public class CookieUtil {
+
+    /**
+     * 设置cookie
+     * @param response
+     * @param name
+     * @param value
+     * @param maxAge
+     */
+    public static void set(HttpServletResponse response,
+                           String name,
+                           String value,
+                           int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
+
+    public static Cookie get(HttpServletRequest request,
+                             String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie: cookies) {
+                if (name.equals(cookie.getName())) {
+                    return cookie;
+                }
+            }
+        }
+
+        return null;
+    }
+
+}
