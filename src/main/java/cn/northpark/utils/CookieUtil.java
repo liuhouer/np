791f81a4b8f11972e6ea4d19c5ea83b74a4f8714
liuhouer <654714226@qq.com>
2018-12-07 14:19:47 +0800

    cookie方法

--- src/main/java/cn/northpark/utils/CookieUtil.java
+++ src/main/java/cn/northpark/utils/CookieUtil.java
@@ -1,5 +1,8 @@
 package cn.northpark.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -65,6 +68,22 @@ public class CookieUtil {
     		}
     	}
 
+    }
+    
+    /**
+     * 将cookie封装成Map
+     * @param request
+     * @return
+     */
+    private static Map<String, Cookie> readCookieMap(HttpServletRequest request) {
+        Map<String, Cookie> cookieMap = new HashMap<>();
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie: cookies) {
+                cookieMap.put(cookie.getName(), cookie);
+            }
+        }
+        return cookieMap;
     }
 
 }
