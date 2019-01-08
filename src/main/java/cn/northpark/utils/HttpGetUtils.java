d9c812274a90dbde6ce97e2c8982f028ce19dae1
liuhouer <654714226@qq.com>
2019-01-08 11:10:25 +0800

    http post

--- src/main/java/cn/northpark/utils/HttpGetUtils.java
+++ src/main/java/cn/northpark/utils/HttpGetUtils.java
@@ -2,20 +2,30 @@ package cn.northpark.utils;/**
  * Created by Administrator on 2017/5/10.
  */
 
-import java.io.BufferedReader;
+ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.http.Consts;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 /**
  * @author caomin
@@ -130,7 +140,7 @@ public class HttpGetUtils {
      * @param charset
      * @return
      */
-    private static String readResponse(HttpEntity resEntity, String charset) {
+    public static String readResponse(HttpEntity resEntity, String charset) {
         StringBuffer res = new StringBuffer();
         BufferedReader reader = null;
         try {
@@ -156,6 +166,68 @@ public class HttpGetUtils {
             }
         }
         return res.toString();
+    }
+
+    
+    
+    /**
+	    *    发送不带参数的HttpPost请求
+	 * @param url
+	 * @return
+	 */
+	public static String sendPost(String url) {
+		//1.获得一个httpclient对象
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		//2.生成一个post请求
+		HttpPost httppost = new HttpPost(url);
+		CloseableHttpResponse response = null;
+		try {
+			//3.执行get请求并返回结果
+			response = httpclient.execute(httppost);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//4.处理结果，这里将结果返回为字符串
+		HttpEntity entity = response.getEntity();
+		String result = null;
+		try {
+			result = EntityUtils.toString(entity);
+		} catch (ParseException | IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+    
+    /**
+	   *    发送HttpPost请求，参数为map
+	 * @param url
+	 * @param map
+	 * @return
+	 */
+    public static String sendPost(String url, Map<String, String> map) {
+    	CloseableHttpClient httpclient = HttpClients.createDefault();
+    	List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+    	for (Map.Entry<String, String> entry : map.entrySet()) {
+    		//给参数赋值
+    		formparams.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+    	}
+    	UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
+    	HttpPost httppost = new HttpPost(url);
+    	httppost.setEntity(entity);
+    	CloseableHttpResponse response = null;
+    	try {
+    		response = httpclient.execute(httppost);
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	HttpEntity entity1 = response.getEntity();
+    	String result = null;
+    	try {
+    		result = EntityUtils.toString(entity1);
+    	} catch (ParseException | IOException e) {
+    		e.printStackTrace();
+    	}
+    	return result;
     }
 
 
