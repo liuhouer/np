ce90ebe5641b419f17fa9f9a975b46ad8dd2537e
liuhouer <654714226@qq.com>
2019-03-26 09:24:08 +0800

    删除没用的方法

--- src/main/java/cn/northpark/utils/JsonUtil.java
+++ src/main/java/cn/northpark/utils/JsonUtil.java
@@ -7,50 +7,14 @@ import java.util.Map;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
-import com.google.common.collect.Lists;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class JsonUtil {
 
-
 	//以下是常用的方法==========================================================================================================
-
-	/**
-	 * list to json
-	 *
-	 * @param list
-	 *          the list that will transform to json string
-	 * @return
-	 *          the json string of list transform
-	 */
-	public static String list2json(List<?> list) {
-		return JSON.toJSONString(list);
-	}
-
-	/**
-	 * map to json
-	 * @param map
-	 *          the map that will transform to json string
-	 * @return
-	 *          the json string of map transform
-	 */
-	public static String map2json(Map<String,Object> map) {
-		return JSONObject.toJSONString(map);
-	}
-
-	/**
-	 * object array to json
-	 *
-	 * @param objects
-	 *          the object array that will transform to json string
-	 * @return
-	 *          the json string of array transform
-	 */
-	public static String array2json(Object[] objects) {
-		return JSON.toJSONString(objects);
-	}
 
 	/**
 	 * object to json
@@ -61,7 +25,7 @@ public class JsonUtil {
 	 *          the json string of object
 	 */
 	public static String object2json(Object object) {
-		return JSON.toJSONString(object);
+		return JSON.toJSONString(object,SerializerFeature.PrettyFormat,SerializerFeature.WriteMapNullValue,SerializerFeature.WriteNullListAsEmpty,SerializerFeature.WriteNullStringAsEmpty,SerializerFeature.WriteBigDecimalAsPlain);
 	}
 
 
@@ -80,6 +44,7 @@ public class JsonUtil {
 	public static <T> List<T> json2list(String json, Class<T> clazz) {
 		return JSON.parseArray(json, clazz);
 	}
+	
 
 	/**
 	 * json to map
@@ -119,30 +84,13 @@ public class JsonUtil {
 	}
 
 	/**
-	 * json string to object
-	 *
-	 * @param json
-	 *          the json string that will transform to object
-	 * @param clazz
-	 *          the class that json will transform
-	 * @param <T>
-	 *          the object class
-	 * @return
-	 *          the object of json string
-	 */
-	public static <T> Object json2object(String json, Class<T> clazz) {
-		return JSON.parseObject(json, clazz);
-	}
-
-
-	/**
 	 * 用fastjson 将json字符串解析为一个 JavaBean
 	 * 
 	 * @param jsonString
 	 * @param cls
 	 * @return
 	 */
-	public static <T> T json2Bean(String jsonString, Class<T> cls) {
+	public static <T> T json2object(String jsonString, Class<T> cls) {
 		T t = null;
 		try {
 			t = JSON.parseObject(jsonString, cls);
@@ -153,48 +101,15 @@ public class JsonUtil {
 		return t;
 	}
 
-	/**
-	 * 用fastjson 将json字符串 解析成为一个 List<JavaBean> 及 List<String>
-	 * 
-	 * @param jsonString
-	 * @param cls
-	 * @return
-	 */
-	public static <T> List<T> json2ListBean(String jsonString, Class<T> cls) {
-		List<T> list = new ArrayList<T>();
-		try {
-			list = JSON.parseArray(jsonString, cls);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		return list;
-	}
-	/**
-	 * 用fastjson 将json字符串 解析成为一个 List<JavaBean> 及 List<String>
-	 * 
-	 * @param jsonString
-	 * @param cls
-	 * @return
-	 */
-	@SuppressWarnings("unchecked")
-	public static <T> List<T> json2ListString(String jsonString) {
-		List<T> list = new ArrayList<T>();
-		try {
-			list = (List<T>) JSON.parseArray(jsonString);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		return list;
-	}
-
+	
 	/**
 	 * 用fastjson 将jsonString 解析成 List<Map<String,Object>>
 	 * 
 	 * @param jsonString
 	 * @return
 	 */
-	public static List<Map<String, Object>> getListMap(String jsonString) {
-		List<Map<String, Object>> list = Lists.newArrayList();
+	public static List<Map<String, Object>> json2ListMap(String jsonString) {
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 		try {
 			// 两种写法
 			// list = JSON.parseObject(jsonString, new
@@ -210,7 +125,6 @@ public class JsonUtil {
 	}
 
 	//以上是常用的方法==========================================================================================================
-
 
 	public static void main(String[] args) {
 		log.error("some thing");
