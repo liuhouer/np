3e44124f7209717e7fcbc6831c620b7580b18121
liuhouer <654714226@qq.com>
2019-07-02 17:00:28 +0800

    rsa encrypt

---
+++ src/main/java/cn/northpark/utils/encrypt/RSAUtils.java
@@ -0,0 +1,98 @@
+package cn.northpark.utils.encrypt;
+import java.nio.charset.Charset;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class RSAUtils {
+	
+	
+	public static final String RSA_ALGORITHM = "RSA";
+    public static final Charset UTF8 = Charset.forName("UTF-8");
+   
+    private static final String PUBLIC_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCPK4VNpHjwojxs5HQoJfMwtvQPOKZ16frV+BqxpAihohCutRQyIfeAu4iwF5CB9rt6Mjh3DdbY5DJ+bgirfsSC43UcqKR4zGAjam/BbZPB+OEBwXxLoYRhILHCagXZdKaWJwlcODgkHpPymjG5Qf5eWyJilZAyAnMVtAD+sEYgYwIDAQAB";  
+    
+
+    /**
+	 * 从字符串中加载公钥
+	 * 
+	 * @param publicKeyStr
+	 *            公钥数据字符串
+	 * @throws Exception
+	 *             加载公钥时产生的异常
+	 */
+	public static PublicKey loadPublicKeyByStr(String publicKeyStr)
+			throws Exception {
+		try {
+			byte[] buffer = Base64.decodeBase64(publicKeyStr);
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			X509EncodedKeySpec keySpec = new X509EncodedKeySpec(buffer);
+			PublicKey generatePublic = keyFactory.generatePublic(keySpec);
+			return generatePublic ;
+		} catch (NoSuchAlgorithmException e) {
+			throw new Exception("无此算法");
+		} catch (InvalidKeySpecException e) {
+			throw new Exception("公钥非法");
+		} catch (NullPointerException e) {
+			throw new Exception("公钥数据为空");
+		}
+	}
+
+	
+	/**
+	 * 公钥加密过程
+	 * 
+	 * @param publicKey
+	 *            公钥
+	 * @param plainTextData
+	 *            明文数据
+	 * @return
+	 * @throws Exception
+	 *             加密过程中的异常信息
+	 */
+	public static byte[] encrypt(PublicKey publicKey, byte[] plainTextData)
+			throws Exception {
+		if (publicKey == null) {
+			throw new Exception("加密公钥为空, 请设置");
+		}
+		Cipher cipher = null;
+		try {
+			// 使用默认RSA
+			cipher = Cipher.getInstance("RSA");
+			// cipher= Cipher.getInstance("RSA", new BouncyCastleProvider());
+			cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+			byte[] output = cipher.doFinal(plainTextData);
+			return output;
+		} catch (NoSuchAlgorithmException e) {
+			throw new Exception("无此加密算法");
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+			return null;
+		} catch (InvalidKeyException e) {
+			throw new Exception("加密公钥非法,请检查");
+		} catch (IllegalBlockSizeException e) {
+			throw new Exception("明文长度非法");
+		} catch (BadPaddingException e) {
+			throw new Exception("明文数据已损坏");
+		}
+	}
+    
+    public static void main(String[] args) throws Exception {
+    	PublicKey pubKey  = loadPublicKeyByStr(PUBLIC_KEY);
+    	String encryptByPublicKey = new String(encrypt(pubKey,"fengkongceshi".getBytes()));
+    	System.out.println(encryptByPublicKey);
+	}
+
+
+}
+
