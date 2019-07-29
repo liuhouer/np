5e75b4bcc4432748942b978661bb5035ca8e69fe
liuhouer <654714226@qq.com>
2019-07-29 15:40:43 +0800

    copyright

--- src/main/java/cn/northpark/utils/StringCommon.java
+++ src/main/java/cn/northpark/utils/StringCommon.java
@@ -1,28 +1,3 @@
-
-/***************************************************************************/
-/*                                                                         */
-/* 			Copyright (c) TAIKANG ASSET MANAGEMENT CO., LTD.               */
-/*                 泰康资产管理有限责任公司    版权所有		                   */
-/*                                                                         */
-/* PROPRIETARY RIGHTS of TAIKANG ASSET MANAGEMENT CO.,LTD. are involved in */
-/* the subject matter of this material.  All manufacturing, reproduction,  */
-/* use, and sales rights pertaining to this subject matter are governed by */
-/* the license agreement.  The recipient of this software implicitly  	   */
-/* accept the terms of the license.                                        */
-/* 本软件文档资料是泰康资产管理有限责任公司的资产,任何人士阅读使用必须获得         */
-/* 相应的书面授权,承担保密责任和接受相应的法律约束.                           */
-/*                                                                         */
-/***************************************************************************/
-/************************************************************
-  Copyright (C), TAIKANG ASSET MANAGEMENT. Co., Ltd.
-  FileName: StringCommon.java
-  Author: zhang yang       Version : 1.0         Date:2018年12月4日
-  Description:     // 模块描述      
-  Version:         // 版本信息
-  History:         // 历史修改记录
-      <author>  <time>   <version >   <desc>
-***********************************************************/
-
 package cn.northpark.utils;
 
 import java.io.IOException;
@@ -208,7 +183,7 @@ public class StringCommon {
 	
 	/**
 	 * 写入文件
-	 * @author w_zhangyang
+	 * @author jeyy
 	 * @param response 
 	 * @param data 写入的内容
 	 * @param fileName 交互？要保存的文件名
