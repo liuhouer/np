a048eeca62476f492682293357871d198767ac4b
liuhouer <zhangyang.z@icloud.com>
2020-08-11 22:39:50 +0800

    movie runner

--- src/test/java/cn/northpark/test/TestEQTask.java
+++ src/test/java/cn/northpark/test/TestEQTask.java
@@ -67,9 +67,6 @@ public class TestEQTask {
 	// @Autowired
 	// public PoemManager poemManager;
 	//
-	//
-	// @Autowired
-	// public PoemEnjoyManager poemenjoyManager;
 
 	public void runTask() {
 
