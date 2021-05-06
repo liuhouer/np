08056dc9f31ff3802e993d6f1758cd49d9a003ae
liuhouer <654714226@qq.com>
2021-05-06 20:05:17 +0800

    观察者模式代码DEMO

---
+++ src/main/java/cn/northpark/pattern/观察者_通知回调模式/Trap.java
@@ -0,0 +1,21 @@
+package cn.northpark.pattern.观察者_通知回调模式;
+
+/**
+ * @author zhangyang
+ * @date 2021年05月06日 20:01:39
+ */
+//陷阱
+public class Trap implements Observer {
+
+    @Override
+    public void update() {
+        if(inRange()){
+            System.out.println("陷阱 困住主角！");
+        }
+    }
+
+    private boolean inRange(){
+        //判断主角是否在自己的影响范围内，这里忽略细节，直接返回true
+        return true;
+    }
+}
