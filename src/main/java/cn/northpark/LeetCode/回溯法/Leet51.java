9263cb7e783e500d8212b5b56794660565de0878
六猴儿 <654714226@qq.com>
2021-07-12 17:05:19 +0800

    51. N 皇后

---
+++ src/main/java/cn/northpark/LeetCode/回溯法/Leet51.java
@@ -0,0 +1,123 @@
+package cn.northpark.LeetCode.回溯法_剪枝;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author zhangyang
+ * @date 2021年07月09日
+ * 51. N 皇后
+ * n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ *
+ * 给你一个整数 n ，返回所有不同的 n 皇后问题 的解决方案。
+ *
+ * 每一种解法包含一个不同的 n 皇后问题 的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：n = 4
+ * 输出：[[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+ * 解释：如上图所示，4 皇后问题存在两个不同的解法。
+ * 示例 2：
+ *
+ * 输入：n = 1
+ * 输出：[["Q"]]
+ *
+ *
+ * 提示：
+ *
+ * 1 <= n <= 9
+ * 皇后彼此不能相互攻击，也就是说：任何两个皇后都不能处于同一条横行、纵行或斜线上。
+ *
+ * ## 回溯三部曲
+ *
+ * 按照我总结的如下回溯模板，我们来依次分析：
+ *
+ * void backtracking(参数) {
+ *     if (终止条件) {
+ *         存放结果;
+ *         return;
+ *     }
+ *     for (选择：本层集合中元素（树中节点孩子的数量就是集合的大小）) {
+ *         处理节点;
+ *         backtracking(路径，选择列表); // 递归
+ *         回溯，撤销处理结果
+ *     }
+ * }
+ *
+ *
+ */
+public class Leet51 {
+
+    List<List<String>> res = new ArrayList<>();
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] chessboard = new char[n][n];
+        for (char[] c : chessboard) {
+            Arrays.fill(c, '.');
+        }
+        backTrack(n, 0, chessboard);
+        return res;
+    }
+
+
+    public void backTrack(int n, int row, char[][] chessboard) {
+        if (row == n) {
+            res.add(Array2List(chessboard));
+            return;
+        }
+
+        for (int col = 0;col < n; ++col) {
+            if (isValid (row, col, n, chessboard)) {//判断条件就是剪枝，而不是随意摆放
+                chessboard[row][col] = 'Q';
+                //保证找到答案的关键代码在这里，如果前面放的位置不合理，就不会row+1直到加到4，也就不会加到最后的结果中去
+                backTrack(n, row+1, chessboard);
+                chessboard[row][col] = '.';
+            }
+        }
+
+    }
+
+
+    public List Array2List(char[][] chessboard) {
+        List<String> list = new ArrayList<>();
+
+        for (char[] c : chessboard) {
+            list.add(String.copyValueOf(c));
+        }
+        return list;
+    }
+
+
+    public boolean isValid(int row, int col, int n, char[][] chessboard) {
+        // 检查列
+        for (int i=0; i<n; ++i) {
+            if (chessboard[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // 检查45度对角线
+        for (int i=row-1, j=col-1; i>=0 && j>=0; i--, j--) {
+            if (chessboard[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // 检查135度对角线
+        for (int i=row-1, j=col+1; i>=0 && j<=n-1; i--, j++) {
+            if (chessboard[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        new Leet51().solveNQueens(4);
+    }
+}
