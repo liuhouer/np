1e4a18bd97259c9c2ca048294f2b6b3292b06be8
liuhouer <654714226@qq.com>
2021-10-29 18:00:40 +0800

    添加mysql、clickhouse多数据源查询数据

---
+++ src/main/java/cn/northpark/constant/DataSourceEnum.java
@@ -0,0 +1,15 @@
+package cn.northpark.constant;
+
+
+/**
+ * NorthPark 动态数据源
+ * >mysql
+ * >clickhouse
+ */
+public interface DataSourceEnum {
+
+    String mysql = "mysql";
+
+    String ck = "ck";
+
+}
