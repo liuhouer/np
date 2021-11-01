6a054284fa78f2912158b32ca171afda33102110
liuhouer <654714226@qq.com>
2021-11-01 16:55:16 +0800

    更新各种场景的电影颜色
    (cherry picked from commit c9500dbc8d217143f154a4054bf7939a1c17dd37)

---
+++ src/test/java/cn/northpark/test/dataclean/updateMovieColor.java
@@ -0,0 +1,53 @@
+package cn.northpark.test.dataclean;
+
+import cn.northpark.constant.BC_Constant;
+import cn.northpark.utils.NPQueryRunner;
+import cn.northpark.utils.PinyinUtil;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.UnmodifiableIterator;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.dbutils.handlers.MapListHandler;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class updateMovieColor {
+
+    public static void main(String[] args) {
+        ImmutableList cList = BC_Constant.specColorMap.keySet().asList();
+        cList.parallelStream().forEach(c->{
+            System.err.println("spec color---->"+c);
+
+            String sql = "select id,moviename from bc_movies  where color = ?";
+            List<Map<String, Object>> maps = NPQueryRunner.query(sql, new MapListHandler(),c);
+
+            //尝试从标题截图赋值
+//            maps.parallelStream().forEach(item->{
+//                String id = item.get("id").toString();
+//                String moviename = item.get("moviename").toString();
+//                for (Object cc : cList) {
+//                    moviename = moviename.replace(String.valueOf(cc),String.valueOf(BC_Constant.specColorMap.get(String.valueOf(cc))));
+//                }
+//                System.err.println("mod moviename---->"+moviename);
+//                if(StringUtils.isNotEmpty(moviename)){
+//
+//                    String color = PinyinUtil.getFirstChar(moviename);
+//                    System.err.println("mod color---->"+color);
+//                    String up_sql = "update bc_movies set color = '"+color+"' where id = "+id;
+//                    NPQueryRunner.update(up_sql);
+//                }
+//            });
+
+            //全是特殊字符的直接替换color,不从标题截取
+            maps.parallelStream().forEach(item-> {
+                String id = item.get("id").toString();
+                String up_sql = "update bc_movies set color = '" + String.valueOf(BC_Constant.specColorMap.get(String.valueOf(c))) + "' where id = " + id;
+                NPQueryRunner.update(up_sql);
+            });
+        });
+    }
+
+}
