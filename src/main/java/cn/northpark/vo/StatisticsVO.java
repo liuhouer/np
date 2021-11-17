ea08bf7c987c96710bb7cf9ccb14bd621cf66c80
liuhouer <654714226@qq.com>
2021-11-17 18:04:38 +0800

    数据埋点-统计登录用户请求的uri

---
+++ src/main/java/cn/northpark/vo/StatisticsVO.java
@@ -0,0 +1,49 @@
+package cn.northpark.vo;
+
+import lombok.Builder;
+import lombok.Setter;
+
+import java.util.Map;
+
+/**
+ * @author bruce
+ * @date 2021年11月17日 16:24:01
+ */
+@Builder
+public class StatisticsVO {
+    String url ;
+    String method ;
+    String ip ;
+    String class_method ;
+    String args ;
+
+    @Setter
+    UserVO userVO;
+
+    @Setter
+    Map<String, String> cookieMap;
+
+    @Override
+    public String toString() {
+
+        String pre = "{" +
+                "url='" + url + '\'' +
+                ", method='" + method + '\'' +
+                ", ip='" + ip + '\'' +
+                ", class_method='" + class_method + '\'' +
+                ", args='" + args + '\'';
+
+        if (userVO != null) {
+            pre += ", userVO=###" + userVO.toString() + "###";
+        }
+        if (cookieMap != null) {
+            pre += ", cookieMap=###" + cookieMap + "###";
+        }
+
+        pre += '}';
+
+        return pre;
+
+
+    }
+}
