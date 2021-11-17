b1b140d3e390525aac97c3febf4771bd7db3e262
liuhouer <654714226@qq.com>
2021-11-17 18:04:38 +0800

    数据埋点-统计登录用户请求的uri

--- src/main/java/cn/northpark/action/NPDispatchServlet.java
+++ src/main/java/cn/northpark/action/NPDispatchServlet.java
@@ -1,12 +1,18 @@
 
 package cn.northpark.action;
 
+import cn.northpark.threadLocal.RequestHolder;
+import cn.northpark.utils.CookieUtil;
+import cn.northpark.vo.StatisticsVO;
+import cn.northpark.vo.UserVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.util.UrlPathHelper;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author bruce
@@ -34,11 +40,22 @@ public class NPDispatchServlet extends DispatcherServlet {
 		      //ip
 		      //类方法-使用joinPoint对象取
 		      //参数
-			log.info("NorthPark处理统计404路径----->{}",request.getRequestURI());
-	        log.info("url={}----->",requestUri);
-	        log.info("method={}----->",request.getMethod());
-	        log.info("ip={}----->",request.getRemoteAddr());
-	        log.info("args={}----->",request.getQueryString());
+			StatisticsVO statisticsVO = StatisticsVO.builder()
+					.url(request.getRequestURI())
+					.method(request.getMethod())
+					.ip(request.getRemoteAddr())
+					.args(request.getQueryString())
+					.build();
+
+			UserVO userInfo = RequestHolder.getUserInfo(request);
+			if(Objects.nonNull(userInfo)){
+				statisticsVO.setUserVO(userInfo);
+
+			}else{
+				Map<String, String> cookieMap = CookieUtil.readCookieUA(request);
+				statisticsVO.setCookieMap(cookieMap);
+			}
+			log.info("[NorthPark处理统计404路径]^"+statisticsVO.toString());
 
 		}
 
--- src/main/java/cn/northpark/aspect/HttpAspect.java
+++ src/main/java/cn/northpark/aspect/HttpAspect.java
@@ -1,40 +1,122 @@
 package cn.northpark.aspect;
-//import javax.servlet.http.HttpServletRequest;
-//
-//import org.aspectj.lang.JoinPoint;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Before;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.context.request.RequestContextHolder;
-//import org.springframework.web.context.request.ServletRequestAttributes;
-//
-///**
-// * @author YangZhang
-// *  可以定义一些切面来对某些方法 进行xx操作
-// *  BRUCE TIPS!
-// */
-//@Aspect
-//@Component
-//public class HttpAspect {
-//
-//    
-//    //cn.northpark.action.UserAction.*(..) public * cn.northpark.action.UserAction.*(..)) &&
-//    @Before("execution(public * cn.northpark.action.UserAction.*(..))")
-//    public void log(JoinPoint  joinPoint){
-//    	//获取request对象的所有属性
-//        ServletRequestAttributes attributes=(ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-//        HttpServletRequest request=attributes.getRequest();
-//	      //url
-//	      //method
-//	      //ip
-//	      //类方法-使用joinPoint对象取
-//	      //参数
+
+import cn.northpark.model.NotifyRemind;
+import cn.northpark.notify.NotifyEnum;
+import cn.northpark.threadLocal.RequestHolder;
+import cn.northpark.threadpool.AsyncThreadPool;
+import cn.northpark.utils.AddressUtils;
+import cn.northpark.utils.CookieUtil;
+import cn.northpark.utils.JsonUtil;
+import cn.northpark.utils.TimeUtils;
+import cn.northpark.vo.StatisticsVO;
+import cn.northpark.vo.UserVO;
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.ImmutableList;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author YangZhang
+ *  可以定义一些切面来对某些方法 进行xx操作
+ *  BRUCE TIPS!
+ */
+@Aspect
+@Component
+@Slf4j
+public class HttpAspect {
+
+
+    public final static ImmutableList<String> PASS_URI =
+            ImmutableList.of("/error","/building");
+
+    //cn.northpark.action.UserAction.*(..) public * cn.northpark.action.UserAction.*(..)) &&
+    @Before("execution(public * cn.northpark.action.*.*(..))")
+    public void log(JoinPoint  joinPoint){
+    	//获取request对象的所有属性
+        ServletRequestAttributes attributes=(ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request=attributes.getRequest();
+	      //url
+	      //method
+	      //ip
+	      //类方法-使用joinPoint对象取
+	      //参数
 //        log.info("url={}----->",request.getRequestURI());
 //        log.info("method={}----->",request.getMethod());
 //        log.info("ip={}----->",request.getRemoteAddr());
 //        log.info("class_method={}----->",joinPoint.getSignature().getDeclaringTypeName()+"."+joinPoint.getSignature().getName());
 //        log.info("args={}----->",request.getQueryString());
-//
-//    }
-//
-//}
+
+        String url = request.getRequestURL().toString();
+        if(!PASS_URI.contains(request.getRequestURI())){
+
+            StatisticsVO statisticsVO = StatisticsVO.builder()
+                    .url(url)
+                    .method(request.getMethod())
+                    .ip(request.getRemoteAddr())
+                    .class_method(joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName())
+                    .args(request.getQueryString())
+                    .build();
+
+            UserVO userInfo = RequestHolder.getUserInfo(request);
+
+            //注册用户
+            if(Objects.nonNull(userInfo)){
+                statisticsVO.setUserVO(userInfo);
+
+                //数据埋点-统计登录用户请求的uri
+                //===================================异步操作=================================================
+                ThreadPoolExecutor threadPoolExecutor = AsyncThreadPool.getInstance().getThreadPoolExecutor();
+                threadPoolExecutor.execute(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        //发送异步站长通知消息
+                        try {
+                            //=================================消息提醒====================================================
+
+                            //判断主题类型
+                            NotifyEnum match = NotifyEnum.WEBMASTER;
+
+                            //提醒系统赋值
+                            NotifyRemind nr = new NotifyRemind();
+
+                            //common
+                            nr.setMessage(userInfo.toString()+"---"+ TimeUtils.nowTime()+"---请求了"+url+"界面---");
+                            nr.setStatus("0");
+
+                            match.notifyInstance.execute(nr);
+
+                            //=================================消息提醒====================================================
+                        }catch (Exception ig){
+                            log.error("统计登录用户请求的uri-notice-has-ignored-------:",ig);
+                        }
+                    }
+
+
+
+                });
+                //===================================异步操作=================================================
+
+
+            }else{
+                //匿名用户
+                Map<String, String> cookieMap = CookieUtil.readCookieUA(request);
+                statisticsVO.setCookieMap(cookieMap);
+            }
+
+            log.info("[Statistics Info]^"+statisticsVO.toString());
+        }
+
+    }
+
+}
