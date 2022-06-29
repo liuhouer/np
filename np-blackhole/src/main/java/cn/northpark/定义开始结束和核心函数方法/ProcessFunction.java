f188dbaaad9c8a5b2c238a3cf00bfa8e3b2ff3b5
liuhouer <654714226@qq.com>
2022-06-29 17:44:13 +0800

    实现Flink模式的open和close函数

--- np-blackhole/src/main/java/cn/northpark/定义开始结束和核心函数方法/ProcessFunction.java
+++ np-blackhole/src/main/java/cn/northpark/定义开始结束和核心函数方法/ProcessFunction.java
@@ -24,5 +24,12 @@ public abstract class ProcessFunction<T> implements ProcessInterface{
         System.err.println("统一执行close------");
     }
 
-    public abstract T call();
+    /**
+     * 注意：这个抽象函数定义域为protected,
+     * 可以防止外部包调用该函数，
+     * 有执行顺序，又需要不同实现的函数，
+     * 不能被外部类直接调用，不然不会执行到前后的顺序(open\close)函数
+     * @return
+     */
+    protected abstract T call();
 }
--- np-blackhole/src/main/java/cn/northpark/定义开始结束和核心函数方法/Test.java
+++ np-blackhole/src/main/java/cn/northpark/定义开始结束和核心函数方法/Test.java
@@ -1,5 +1,7 @@
 package cn.northpark.定义开始结束和核心函数方法;
 
+import cn.northpark.定义开始结束和核心函数方法.ProcessFunction;
+
 /**
  * @author bruce
  * @date 2022年06月21日 16:51:09
@@ -15,10 +17,12 @@ public class Test {
             }
         };
 
-        String process = processFunction1.process();
-        System.err.println("执行结果1--->"+process);
+//        String process = processFunction1.process();
+//        System.err.println("执行结果1--->"+process);
+        String call = processFunction1.call();
+        System.err.println("call 1--->"+call);
 
-        //1.执行1
+        //2.执行2
         ProcessFunction<String> processFunction2 = new ProcessFunction<String>() {
             @Override
             public void open() {
